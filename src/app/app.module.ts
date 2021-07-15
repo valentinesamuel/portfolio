9306f2941fa62be246cb9484b6a0a4fc29dd6f1b
Samuel Valentine <valentinesamuel2580@gmail.com>
2021-07-16 00:30:44 +0100

    feat: finished intro section and about me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArchiveProjectsComponent } from './archive-projects/archive-projects.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArchiveProjectsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/archive-projects/archive-projects.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-archive-projects',
+  templateUrl: './archive-projects.component.html',
+  styleUrls: ['./archive-projects.component.scss']
+})
+export class ArchiveProjectsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
