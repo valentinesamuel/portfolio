d99fb40ac2f173e4ef8ab5478bd47e03ec35f159
Samuel Valentine <valentinesamuel2580@gmail.com>
2021-07-16 09:06:52 +0100

    fix: corrected theme colors

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  darkTheme = true;
+  darkTheme = false;
   constructor() { }
 
   ngOnInit(): void {
