18110571840202725e6ecaf1f5729a97e19814c5
Samuel Valentine <valentinesamuel2580@gmail.com>
2021-07-20 08:42:36 +0100

    feat: finished the experienece section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   darkTheme = true;
-  company: any = 'mission';
+  company: any = 'freelance';
   selected
 
   constructor() { }
