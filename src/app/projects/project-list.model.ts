b138cf59077aaf9eaa22b15ad0c6336ac7fee45f
valentinesamuel <valentinesamuel2580@gmail.com>
2021-07-23 16:53:21 +0100

    feat: Made the project image to be in an img tag instead of after psuedo selector

---
+++ src/app/projects/project-list.model.ts
@@ -0,0 +1,11 @@
+export interface Project {
+      name: string;
+      stack: string[];
+      repo: string;
+      liveDemo: string;
+}
+export interface ArchivedProject{
+      name: string;
+      repo: string;
+      
+}
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,13 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { ArchivedProject, Project } from './project-list.model';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
+
+
 export class ProjectsComponent implements OnInit {
   seeOthers = true;
-
+  projectList: Project[] = [
+    {
+      name: 'Project ',
+      stack: ['HTML', 'CSS'],
+      repo: 'git-sample1.com',
+      liveDemo: 'project1.com'
+    },
+    {
+      name: 'Project ',
+      stack: ['JS', 'TS'],
+      repo: 'git-sample2.com',
+      liveDemo: 'project2.com'
+    },
+    {
+      name: 'Project ',
+      stack: ['BASH', 'UID'],
+      repo: 'git-sample3.com',
+      liveDemo: 'project3.com'
+    },
+  ]
+  archivedList: ArchivedProject[] = [
+    {
+      name: 'yehhh',
+      repo:'yehh.github.com'
+    },
+    {
+      name: 'yewrthe',
+      repo:'yewrthe.github.com'
+    },
+    {
+      name: 'sldfib9mgtfb',
+      repo:'sldfib9mgtfb.github.com'
+    },
+    {
+      name: 'slkdmf',
+      repo:'slkdmf.github.com'
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
