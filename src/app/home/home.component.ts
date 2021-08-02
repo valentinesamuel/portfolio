9ff44fd7a11da972257bd3ac553f9127ca424d9c
valentinesamuel <valentinesamuel2580@gmail.com>
2021-08-02 09:54:55 +0100

    fix: fixed the hamburger meuu toggle

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormControlDirective, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,8 +7,12 @@ import { FormControl, FormControlDirective, FormGroup, Validators } from '@angul
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  // @ViewChild('menu') mobMenu: ElementRef;
+  @ViewChild('hamBtn') hamButton: ElementRef;
+  mobMenu
   contactForm: FormGroup;
   darkTheme = true;
+  mobileMenu;
 
   company: any = 'freelance';
   selected
@@ -29,9 +33,15 @@ export class HomeComponent implements OnInit {
     this.selected = !this.selected;
   }
 
+  showMobileMenu() {
+    this.mobileMenu = !this.mobileMenu;
+    if (this.hamButton.nativeElement.classList.contains('open')) {
+      this.mobMenu = !this.mobMenu;
+    }
+  }
 
   onSubmit(form) {
-    
+
     console.log(form);
   }
 
@@ -42,23 +52,31 @@ export class HomeComponent implements OnInit {
 
   }
 
+  // checkUser() {
+  //   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|CriOS/i.test(this.device)) {
+  //     this.mobileMenu=true
+  //   } else {
+
+  //   }
+  // }
+
   nameErrorAlert() {
     const nameField = this.contactForm.get('name');
     if (nameField.status == "INVALID" && nameField.touched == true && nameField.dirty == true && nameField.value != '') {
       return 'Please enter a valid name ';
     }
-    }
+  }
   emailErrorAlert() {
     const emailField = this.contactForm.get('email');
     if (emailField.status == "INVALID" && emailField.touched == true && emailField.dirty == true && emailField.value != '') {
       return 'Please enter a valid email ';
     }
-    }
+  }
   messageErrorAlert() {
     const messageField = this.contactForm.get('message');
     if (messageField.status == "INVALID" && messageField.touched == true && messageField.dirty == true && messageField.value != '') {
       return 'Please add a few more details';
     }
-    }
+  }
 
 }
